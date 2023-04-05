871fccb5f9519ce46f48361a8944801f09579529
kugooer <kugooer@users.noreply.github.com>
2023-04-05 13:20:26 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,13 +2,13 @@ module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
     siteTitle: 'Running Page',
-    siteUrl: 'https://run.yoyozai.com',
+    siteUrl: 'https://www.yoyozai.com/running_page',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
       {
         name: 'Blog',
-        url: 'https://run.yoyozai.com/running',
+        url: 'https://www.yoyozai.com/',
       },
       {
         name: 'About',
